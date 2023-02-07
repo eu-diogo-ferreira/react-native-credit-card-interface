3e28a4f74a1c6d8130d136499d50a0a22a817a58
Diogo Ferreira <diogocraft.2@gmail.com>
2023-02-07 01:51:13 -0300

    feat(components): desenvolvidos componentes base da aplicação

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Container, Title, Subtitle, Content, Button, TextButton } from "./styles";
+
+const Home = () => {
+  return (
+    <Container>
+      <Title>Meu Cartão</Title>
+      <Subtitle>Insira os dados do cartão</Subtitle>
+
+      <Content>
+
+      </Content>
+
+      <Button>
+        <TextButton>Cadastrar</TextButton>
+      </Button>
+    </Container>
+  );
+}
+
+export default Home;
---
+++ src/screens/Home/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View``;
+
+export const Title = styled.Text``;
+
+export const Subtitle = styled.Text``;
+
+export const Content = styled.View``;
+
+export const Button = styled.TouchableOpacity``;
+
+export const TextButton = styled.Text``;
