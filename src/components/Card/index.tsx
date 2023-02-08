3d438c32fa5fcdda220dea9b912f1e75326a710a
Diogo Ferreira <diogocraft.2@gmail.com>
2023-02-07 23:58:04 -0300

    feat(components): `Card` desenvolvido componente de cartão

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 
-import { Container, Content } from "./styles";
+import { Container, Content, Strip, TextCVV, ViewInformation, View, Text } from "./styles";
 
-const Card = () => {
+const Card = ({ data, back, icon }) => {
   return (
     <Container>
       <Content>
+        {
+          back
+            ? <Strip>
+              <TextCVV>{data.cvv}</TextCVV>
+            </Strip>
+            : <ViewInformation>
+              <View>
+                <Text bold={true} fontSize="16px">{data.number}</Text>
+                <Text fontSize="14px">{data.name}</Text>
+                <Text fontSize="12px">{data.validate}</Text>
+              </View>
+              {icon && icon}
+            </ViewInformation>
+        }
       </Content>
     </Container>
   );
--- src/components/Card/styles.tsx
+++ src/components/Card/styles.tsx
@@ -12,3 +12,47 @@ export const Content = styled.View`
   background-color: #6a1b9a;
   border-radius: 20px;
 `;
+
+export const Strip = styled.View`
+  width: 100%;
+  height: 30px;
+  margin-top: 25px;
+  background-color: #BDBDBD;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+`;
+
+export const TextCVV = styled.Text`
+  margin-right: 40px;
+`;
+
+export const ViewInformation = styled.View`
+  width: 100%;
+  padding: 14px;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+`;
+
+export const View = styled.Text`
+  width: 80%;
+  margin-top: 50px;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+
+interface propsText {
+  fontSize: string,
+  bold?: boolean
+}
+
+export const Text = styled.Text<propsText>`
+  width: auto;
+  max-height: 35px;
+  margin-top: 8px;
+  color: #FAFAFA;
+  font-size: ${({ fontSize }) => fontSize ? fontSize : '14px' };
+  font-weight: ${({ bold }) => bold ? 'bold' : 'normal' };
+`;
